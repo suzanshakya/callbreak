015d29d32c0790326b4afee84579351112330d6c
Sujan Shakya <suzan.shakya@gmail.com>
2013-09-24 17:15:17 +0545

    refactored: moved dimension manipulation to set_dimension method

--- main.py
+++ main.py
@@ -96,45 +96,55 @@ class CardUI:
 
 
 class PlayerUI:
-    cards_v_spacing = 30
-    cards_h_spacing = 35
-
-    def __init__(self, player, screen, board, center_position, orientation, hide=False):
+    def __init__(self, player, screen, board, orientation, hide=False):
         player.ui = self
 
         self.player = player
         self.screen = screen
         self.board = board
-        self.center_position = center_position
         self.orientation = orientation
+        self.hide = hide
 
-        if orientation in ('top', 'bottom'):
-            self.horizontally = True
-        elif orientation in ('left', 'right'):
-            self.horizontally = False
+        self.dirty_rects = []  # set by display()
+        self.rect = None  # set by display(), union rect of this players card
+        self.corner_position = None  # set by set_dimensions()
+        self.throw_position = None  # set by set_dimenstions()
+
+        self.hidden_card_rect = load_image(get_back_image()).get_rect()  # used for its dimension
+        self.visible_card_rect = load_image(get_card_image()).get_rect()  # used for its dimension
+        self.set_dimensions()
+
+    @property
+    def card_rect(self):
+        if self.hide:
+            return self.hidden_card_rect
+        else:
+            return self.visible_card_rect
+
+    def set_dimensions(self):
+        self.cards_v_spacing = 10 if self.hide else 30
+        self.cards_h_spacing = 10 if self.hide else 50
+
+        _pad = 5
+        padding = {'left': _pad, 'top': _pad, 'right': _pad, 'bottom': _pad}  # top, right, bottom, left
+
+        if self.orientation == 'left':
+            position = padding['left'], (self.board[1] - self.card_rect.height)/2
+            throw_position = self.board[0]/2 - self.card_rect.width, 0
+        elif self.orientation == 'top':
+            position = (self.board[0] - self.card_rect.width)/2, padding['top']
+            throw_position = 0, self.board[1]/2 - self.card_rect.height
+        elif self.orientation == 'right':
+            position = self.board[0] - padding['right'] - self.card_rect.width, \
+                        (self.board[1] - self.card_rect.height)/2
+            throw_position = self.card_rect.width  - self.board[0]/2, 0
+        elif self.orientation == 'bottom':
+            position = (self.board[0] - self.card_rect.width)/2, self.board[1] - padding['bottom'] - self.card_rect.height
+            throw_position = 0, self.card_rect.height - self.board[1]/2
         else:
             raise Exception("Orientation %r is not supported." % orientation)
-
-        self.hide = hide
-        self.card_rect = load_image(get_card_image()).get_rect()  # used for its dimension
-        self.throw_rect = self.calculate_throw_rect()
-        self.dirty_rects = []
-        self.rect = None  # set by display(), union rect of this players card
-
-        if not hide:
-            self.cards_v_spacing = 30
-            self.cards_h_spacing = 35
-
-    def calculate_throw_rect(self):
-        offsets = {
-            'left': (self.board[0]/2 - self.card_rect.width, 0),
-            'top': (0, self.board[1]/2 - self.card_rect.height),
-            'right': (self.card_rect.width  - self.board[0]/2, 0),
-            'bottom': (0, self.card_rect.height - self.board[1]/2)
-        }
-        position = map(sum, zip(self.center_position, offsets[self.orientation]))
-        self.card_rect.x, self.card_rect.y = position
-        return self.card_rect
+        self.corner_position = position
+        self.throw_position = map(sum, zip(self.corner_position, throw_position))
 
     def collidepoint(x, y):
         return self.rect.collidepoint(x, y)
@@ -143,7 +153,7 @@ class PlayerUI:
         return self.rect.colliderect(rect)
 
     def throw(self, card, turn):
-        card.ui.move(self.throw_rect, callback=self.display, delay=0.1)
+        card.ui.move(self.throw_position, callback=self.display, delay=0.1)
         for c in turn.cards:
             c.ui.redraw()
         card.ui.redraw()
@@ -151,7 +161,7 @@ class PlayerUI:
 
     def collect(self, cards):
         for card in cards:
-            card.ui.move(self.center_position, callback=self.display, delay=0.1, disappear=True)
+            card.ui.move(self.corner_position, callback=self.display, delay=0.1, disappear=True)
 
     def clear_thrown(self):
         self.card_rect.x, self.card_rect.y = self.throw_position
@@ -161,19 +171,19 @@ class PlayerUI:
     def display(self):
         self.dirty_rects[:] = []
         total_cards = len(self.player.all_cards)
-        if self.horizontally:
-            x = self.center_position[0] - (total_cards - 1) / 2 * self.cards_h_spacing
-            y = self.center_position[1]
+        if self.orientation in ('top', 'bottom'):
+            x = self.corner_position[0] - (total_cards - 1) / 2 * self.cards_h_spacing
+            y = self.corner_position[1]
         else:
-            x = self.center_position[0]
-            y = self.center_position[1] - (total_cards - 1) / 2 * self.cards_v_spacing
+            x = self.corner_position[0]
+            y = self.corner_position[1] - (total_cards - 1) / 2 * self.cards_v_spacing
 
         for card in self.player.all_cards:
             cardui = hasattr(card, 'ui') and card.ui or CardUI(card, self.screen)
             rect = cardui.display((x, y), hide=self.hide)
             self.dirty_rects.append(rect)
 
-            if self.horizontally:
+            if self.orientation in ('top', 'bottom'):
                 x += self.cards_h_spacing
             else:
                 y += self.cards_v_spacing
@@ -250,7 +260,7 @@ class CallBreakUI:
             self.resolution = (800, 600)
             self.resolution = (540, 960)
 
-        self.board = self.resolution[0], self.resolution[1] * 0.8
+        self.board = self.resolution[0] * 0.8, self.resolution[1]
         pygame.time.set_timer(pygame.USEREVENT, int(1000 / FPS))
 
         self.screen = pygame.display.set_mode(self.resolution)
@@ -266,20 +276,10 @@ class CallBreakUI:
         player3 = Player('Santosh', is_bot=True)
         player4 = Player('Rupa', is_bot=False)
 
-        card_rect = load_image(get_card_image()).get_rect()
-        padding = {'left': 20, 'top': 20, 'right': 20, 'bottom': 20}  # top, right, bottom, left
-
-        position = (padding['left'], (self.board[1] - card_rect.height)/2)
-        player1_ui = PlayerUI(player1, self.screen, self.board, position, 'left', hide=True)
-
-        position = ((self.board[0] - card_rect.width)/2, padding['top'])
-        player2_ui = PlayerUI(player2, self.screen, self.board, position, 'top', hide=True)
-
-        position = (self.board[0] - padding['right'] - card_rect.width, (self.board[1] - card_rect.height)/2)
-        player3_ui = PlayerUI(player3, self.screen, self.board, position, 'right', hide=True)
-
-        position = ((self.board[0] - card_rect.width)/2, self.board[1] - padding['bottom'] - card_rect.height)
-        player4_ui = PlayerUI(player4, self.screen, self.board, position, 'bottom', hide=False)
+        player1_ui = PlayerUI(player1, self.screen, self.board, 'left', hide=True)
+        player2_ui = PlayerUI(player2, self.screen, self.board, 'top', hide=True)
+        player3_ui = PlayerUI(player3, self.screen, self.board, 'right', hide=True)
+        player4_ui = PlayerUI(player4, self.screen, self.board, 'bottom', hide=False)
 
         players = [player1_ui, player2_ui, player3_ui, player4_ui]
         game = CallBreak([ui.player for ui in players])
@@ -289,7 +289,6 @@ class CallBreakUI:
 
             for player in players:
                 player.display()
-
             pygame.display.update()
 
             game.start()
